684d32baa14ffa5e65a672503f1086778673f178
wydra76yan <yan@vydra.com>
2017-10-07 17:54:58 +0300

    Cherry pick

--- app/src/main/java/com/example/yanvydra/androidhw2/MainActivity.java
+++ app/src/main/java/com/example/yanvydra/androidhw2/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.yanvydra.androidhw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String Tag = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(Tag, "Cherry pick");
         //change 000
         //change 165
         //change 112
