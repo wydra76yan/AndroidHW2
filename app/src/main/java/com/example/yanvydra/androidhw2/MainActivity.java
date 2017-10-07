3b6871bbc9db1608f6958b0c3498f957a0fa09f5
wydra76yan <yan@vydra.com>
2017-10-07 17:54:58 +0300

    Revert "Cherry picked"
    This reverts commit 0653258bdcffba94723662182b92f54fe6faf56c.

--- app/src/main/java/com/example/yanvydra/androidhw2/MainActivity.java
+++ app/src/main/java/com/example/yanvydra/androidhw2/MainActivity.java
@@ -2,25 +2,12 @@ package com.example.yanvydra.androidhw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String Tag = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(Tag, "Cherry pick");
-        //change 000
-        //change 165
-        //change 112
-        //change 111
-        //change 132
-        //change 145
-        //change 156
-        //change 178
-        //change 190
     }
 }
